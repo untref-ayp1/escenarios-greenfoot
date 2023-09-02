cf54985bf99461d84e37917f50d456edc0fd0d0b
Lucas Videla <videla.lucas@gmail.com>
2023-09-02 20:19:53 -0300

    Minor changes on Escenario00

--- Escenario00/Mundo00.java
+++ Escenario00/Mundo00.java
@@ -2,33 +2,31 @@ import greenfoot.*;
 
 public class Mundo00 extends MundoBase {
 
-	public Mundo00() {
-		super(6, 5);
-	}
+    public Mundo00() {
+        super(6, 5, 100);
+    }
 
-	protected void generarNaves() {
-		agregar(new NaveExploradora(), 1, 0);
-		agregar(new NaveDeAtaque(), 2, 1);
-		agregar(new NaveRecolectora(), 3, 2);
+    protected void generarNaves() {
+        agregar(new NaveExploradora(), 1, 0);
+        agregar(new NaveDeAtaque(), 2, 1);
+        agregar(new NaveRecolectora(), 3, 2);
 
-		agregar(new NaveExploradoraEnemiga(Direccion.NORTE), 4, 3);
-		agregar(new NaveDeAtaqueEnemiga(Direccion.NORTE), 5, 4);
+        agregar(new NaveExploradoraEnemiga(Direccion.NORTE), 4, 0);
+        agregar(new NaveDeAtaqueEnemiga(Direccion.NORTE), 5, 1);
 
-		agregar(new Piloto00(), 0, 4);
-	}
+        agregar(new Piloto00(), 0, 4);
+    }
 
-	protected void generarPOIs() {
-		marcarCelda(0, 0, new Color(0, 0, 200, 150));
+    protected void generarPOIs() {
+        marcarCelda(0, 0, new Color(0, 0, 200, 150));
+    }
 
-		agregar(new Brujula(), 5, 1);
-	}
+    protected void generarItems() {
+        agregar(new Item(), 0, 1);
+    }
 
-	protected void generarItems() {
-		agregar(new Item(), 0, 1);
-	}
-
-	protected void generarAsteroides() {
-		agregar(new Asteroide(), 0, 2);
-		agregar(new Mineral(), 0, 3);
-	}
+    protected void generarAsteroides() {
+        agregar(new Asteroide(), 0, 2);
+        agregar(new Mineral(), 0, 3);
+    }
 }
